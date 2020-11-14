067956faae1c58b73e0eea6a3a487e539e2042c0
Michael Barth <sakenda1207@gmail.com>
2020-11-14 20:49:56 +0100

    Projektdateien hinzufügen.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Calculator.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Mathematics mathematics = new Mathematics();
+            mathematics.Input = 100.25;
+
+            Binding bind = new Binding(nameof(mathematics))
+            {
+                Source = mathematics.Input,
+                Mode = BindingMode.TwoWay,
+                //UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            };
+
+            TextBox_Result.SetBinding(TextBox.TextProperty, bind);
+        }
+    }
+}
